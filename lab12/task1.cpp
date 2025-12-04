66af9b03576d17288a7f99158768ed34ae50718b
Saad Tauseef <126574907+DAAS69@users.noreply.github.com>
2025-12-04 23:17:34 +0500

    Update task1.cpp
    Implemented a brute force string matching algorithm to find occurrences of a pattern in a given text.

--- lab12/task1.cpp
+++ lab12/task1.cpp
@@ -1,1 +1,27 @@
-#include<iostream>
+#include <iostream>
+using namespace std;
+
+
+int bruteforce(string text,string pattern){
+	int n  = text.length();
+	int m = pattern.length();
+	
+	for (int i=0;i<=n-m;i++){
+		
+		int j=0;
+		while (j<m && text[i+j]== pattern[j]){
+			j++;
+		}
+		if (j==m){
+			cout <<i<<" ";
+		}
+	}
+}
+int main(){
+	string text = "the quick brown fox jumps over the lazy dog";
+	string pattern = "the";
+	cout <<"Found at following Index: ";
+	bruteforce(text,pattern);
+	
+	
+}
